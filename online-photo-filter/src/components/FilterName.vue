3b86731a1dda30fe312b0865a86f4413ee380a13
Arron McCrory <arronmccrory@gmail.com>
2024-09-26 15:26:33 -0400

    refactoring

---
+++ online-photo-filter/src/components/FilterName.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const displayName = ref()
+const props = defineProps<{
+  filterName: string
+}>()
+
+onMounted(() => {
+  displayName.value = props.filterName
+})
+
+</script>
+
+<template>
+  <div class="filter-name">
+    {{ displayName }}
+  </div>
+</template>
+
+<style scoped>
+.filter-name {
+  font-weight: 500;
+}
+</style>
